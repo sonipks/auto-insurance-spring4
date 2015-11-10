50287faebaf609f7e7c66026a578885aa12e7a8a
Pankaj Soni <sonipks@gmail.com>
2015-11-10 14:13:28 +0530

    added bootstrap framework
    using horizontal login form from w3schools.com

--- src/main/java/com/pks/insurance/domain/User.java
+++ src/main/java/com/pks/insurance/domain/User.java
@@ -11,7 +11,7 @@ import java.util.List;
  * 
  */
 @Entity
-@Table(name="ja_users")
+@Table(name="users")
 @NamedQuery(name="User.findAll", query="SELECT u FROM User u")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
